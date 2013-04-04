9ed007f553b05cfd5206e9a2849d255dc334e69d
helicom <helicoid69@gmail.com>
2013-04-04 19:41:30 +0900

    Navigation Button (under 600px) Design Improvement

--- index.php
+++ index.php
@@ -16,15 +16,13 @@
 
 get_header(); ?>
 
+<div id="primary" class="site-content">
+    <div id="content" role="main">
+        <p>THIS IS THE TOP PAGE.</p>
+    </div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
-
-
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-            <p>THIS IS THE TOP PAGE.</p>
-		</div><!-- #content -->
-	</div><!-- #primary -->
 
 
 <?php get_footer(); ?>
